bffc2642919d787ace3563e38a9d79fca11de2ba
dongjay00 <cjswodlehdgn@gmail.com>
2022-01-31 22:12:25 +0900

    [feat]: Appbar, Sidebar 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  createTheme,
+  CssBaseline,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
+import { AppBarComponent, SideBar, Menu } from "./components";
+import { ROUTES } from "./routes";
 
-function App() {
+export const App: React.FC = (): JSX.Element => {
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("dark");
+  const [sideBarToggle, setSideBarToggle] = useState<boolean>(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode,
+    },
+    typography: {
+      fontSize: 14,
+    },
+  });
+
+  const handleDrawerToggle = React.useCallback(() => {
+    setSideBarToggle(!sideBarToggle);
+  }, [sideBarToggle]);
+
+  const onThemeChange = React.useCallback(() => {
+    setThemeMode(themeMode === "dark" ? "light" : "dark");
+  }, [themeMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBarComponent
+        handleDrawerToggle={handleDrawerToggle}
+        onThemeChange={onThemeChange}
+        isDarkMode={themeMode === "dark"}
+        isDrawerOpen={sideBarToggle}
+      />
+      <SideBar
+        isOpen={sideBarToggle}
+        handleDrawerToggle={handleDrawerToggle}
+        children={<Menu links={ROUTES} />}
+      />
+    </ThemeProvider>
   );
-}
-
-export default App;
+};
---
+++ src/components/Appbar/AppBarComponent.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Switch as ThemeSwitch,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+
+interface Props {
+  handleDrawerToggle: () => void;
+  onThemeChange: () => void;
+  isDarkMode: boolean;
+  isDrawerOpen: boolean;
+}
+
+export const AppBarComponent: React.FC<Props> = ({
+  handleDrawerToggle,
+  onThemeChange,
+  isDarkMode,
+  isDrawerOpen,
+}): JSX.Element => {
+  return (
+    <div>
+      <AppBar position="static" variant="elevation" className="appbar-wrapper">
+        <Toolbar variant="dense">
+          <IconButton
+            color="inherit"
+            onClick={handleDrawerToggle}
+            edge="start"
+            sx={{ mr: 2, ...(isDrawerOpen && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
+            React Repo Trading App
+          </Typography>
+          {isDarkMode ? <DarkModeIcon /> : null}
+          <ThemeSwitch size="small" onChange={onThemeChange} color="default" />
+          {!isDarkMode ? <LightModeIcon /> : null}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
